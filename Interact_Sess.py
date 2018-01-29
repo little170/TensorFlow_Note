0aa8f0b8481536725a27e7c744ce99e632026cb5
little170 <34448467+little170@users.noreply.github.com>
2018-01-29 13:16:59 +0800

    Update Interact_Sess.py

--- Interact_Sess.py
+++ Interact_Sess.py
@@ -1,4 +1,4 @@
-# 进入一个交互式 TensorFlow 会话.
+# 進入一个交互式 TensorFlow 會話
 import tensorflow as tf
 sess = tf.InteractiveSession()
 
@@ -8,7 +8,7 @@ a = tf.constant([3.0, 3.0])
 # 使用初始化器 initializer op 的 run() 方法初始化 'x' 
 x.initializer.run()
 
-# 增加一个减法 sub op, 从 'x' 减去 'a'. 运行减法 op, 输出结果 
+# 增加一个减法 sub op, 从 'x' 减去 'a'. 運行减法 op, 輸出结果 
 sub = tf.subtract(x, a)
 print (sub.eval())
 # ==> [-2. -1.]
